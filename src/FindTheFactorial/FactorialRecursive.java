2cfc4f36ec6ec55598bf773a2a10353f6f2bd484
HitaishiPatel <hitaiship.724@gmail.com>
2021-11-26 18:16:50 -0700

    Factorial Code

---
+++ src/FindTheFactorial/FactorialRecursive.java
@@ -0,0 +1,19 @@
+package FindTheFactorial;
+
+public class FactorialRecursive {
+	
+public static void main(String[] args) {
+	
+		System.out.println(factorial(6));
+	}
+	
+	public static int factorial(int n) {
+		if(n == 1) {
+			System.out.println("factorial(" + n + ") = 1");
+			return 1;
+		} else {
+			System.out.println("factorial(" + n + ") = " + n + " * factorial(" + (n - 1) + ")");
+			return n * factorial(n - 1);
+		}
+	}
+}
---
+++ src/FindTheFactorial/FindTheFactorial.java
@@ -0,0 +1,18 @@
+package FindTheFactorial;
+
+public class FindTheFactorial {
+
+	public static void main(String[] args) {
+		
+		int number = 5;  
+		long factorial = 1; 
+		
+		for(int i=1; i<=number; i++) 
+			
+			factorial = factorial*i; {
+		{
+			System.out.println(factorial);
+		}
+	}
+	}
+}
